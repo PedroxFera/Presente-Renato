ed8cafc96904548f1f452e83ed95e0f8a53e4c4c
PedroxFera <jjp40264@gmail.com>
2026-01-19 21:17:19 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById("video");
-const letter = document.getElementById("letter");
-
-video.addEventListener("ended", () => {
-  document.body.classList.remove("no-scroll");
-  letter.style.display = "block";
-  letter.scrollIntoView({ behavior: "smooth" });
-});
-
